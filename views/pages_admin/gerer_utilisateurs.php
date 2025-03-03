979670fa15cab4ae009957aed8990d861fc1938b
ENS-Studio <nath.affog@gmail.com>
2025-03-03 21:36:29 +0100

    ADD functions admin ADD better css

--- views/pages_admin/gerer_utilisateurs.php
+++ views/pages_admin/gerer_utilisateurs.php
@@ -2,7 +2,6 @@
 require_once '../../includes/db.php';
 require_once '../../includes/admin_functions.php';
 require_once '../../includes/user_functions.php';
-
 if (session_status() == PHP_SESSION_NONE) {
     session_start();
 };
@@ -24,12 +23,12 @@ if (session_status() == PHP_SESSION_NONE) {
         include "../../includes/navbar.php";
         echo createHeader();
     ?>
-    <h1>Panneau d'administration</h2>
+    <h1>Panneau d'administration</h1>
     <p>Bienvenue, <?php echo $_SESSION["username"] ?> !</p>
     <a href="dashboard.php">Retour au tableau de bord</a>
-    <a href="gerer_user.php">Gérer les utilisateurs</a>
+    <a href="gerer_utilisateurs.php">Gérer les utilisateurs</a>
     <a href="categories.php">Gérer les catégories</a>
-    <a href="tasks.php">Gérer les tâches</a>
+    <a href="users_taks.php">Gérer les tâches</a>
     <a href="comments.php">Gérer les commentaires</a>
     <a href="attachments.php">Gérer les pièces jointes</a>
     <a href="logout.php">Déconnexion</a>
